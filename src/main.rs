60cf70c10a1bb1489c665d02e521a6a515f27db1
teuram <vivod.pop@gmail.com>
2023-05-12 14:15:34 +0300

    add new fetures

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,55 @@ use std::{
     io::{BufReader, BufRead}
 };
 
+fn color_f32(num: f32) -> String {
+    format!("{}{:.2}\x1b[1;0m",
+            {
+                if num < 0.0 {
+                    "\x1b[1;31m"
+                } else if num > 0.0 {
+                    "\x1b[1;32m"
+                } else {
+                    "--"
+                }
+            }, num)
+}
+
 fn moni(file: File) {
     let reader = BufReader::new(file);
 
     let mut sum: f32 = 0.0;
     let mut num: f32;
+    let mut values: Vec<f32> = Vec::<f32>::new();
 
     for line in reader.lines() {
 
         let line = line.unwrap().trim().to_string();
 
         if line.eq("-- total") {
-            println!("{:9.2}\n", sum);
+            println!("\n{}\ntotal: {}\n", line, color_f32(sum));
+            continue;
+        }
+        if line.eq("-- push") {
+            values.push(sum);
+            sum = 0.0;
+            println!("{}\n", line);
+            continue;
+        }
+        if line.eq("-- pop") {
+            if let Some(val) = values.pop() {
+                sum += val;
+                println!("{}", line);
+                if sum - val < val {
+                    println!("\x1b[1;33mwarning:\x1b[0m {:.2} is less than zero\n", sum - val);
+                }
+                // if sum + val < val {
+                //     sum += val;
+                //     println!("\x1b[1;33mwarning:\x1b[0m {:.2} is less than zero\n", sum - val);
+                // } else {
+                //     sum += val;
+                //     println!("{}\n", line);
+                // }
+            }
             continue;
         }
 
@@ -28,9 +65,9 @@ fn moni(file: File) {
         println!("{:9.2} {} {:.2}\x1b[0m" , sum,
                  {
                      if num < 0.0 {
-                         "\x1b[38;2;255;0;0m->"
+                         "\x1b[1;31m->"
                      } else if num > 0.0 {
-                         "\x1b[38;2;0;255;0m<+"
+                         "\x1b[1;32m<+"
                      } else {
                          "--"
                      }
